10ce79718f94164237b65335c9ad11018d315bbb
Vipul332 <pandeyvipul883@gmail.com>
2026-04-14 18:23:40 +0530

    added root route

--- server/index.js
+++ server/index.js
@@ -11,6 +11,11 @@ mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log('MongoDB Connected'))
 .catch(err=>console.log(err));
 
+app.get('/', (req, res) => {
+  res.send('API is running 🚀');
+});
+
+
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/user', require('./routes/user'));
 
