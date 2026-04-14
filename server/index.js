a9fb189f8ccf9ea07726f1033be4d7fdc6dd9d5e
Vipul332 <pandeyvipul883@gmail.com>
2026-04-14 17:46:06 +0530

    fix port for deployment

--- server/index.js
+++ server/index.js
@@ -13,4 +13,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/user', require('./routes/user'));
-app.listen(5000, ()=>console.log('Server running'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
